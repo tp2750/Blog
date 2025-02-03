a859d486f360ddcd286b3ce5655c90976ea7bb3e
Thomas Poulsen <ta.poulsen@gmail.com>
2025-02-03 19:25:55 +0100

    longer slider

--- en/2025/2025-01-31_shiny-1/app.R
+++ en/2025/2025-01-31_shiny-1/app.R
@@ -7,13 +7,16 @@ t <- seq(0,1,0.001)
 
 ui <- fluidPage(h1("Hello Shiny"),
                 plotOutput("plot1"),
-                sliderInput("h", label="Frequency", min = 1, max = 10, value= 1)
+                fluidRow(column(
+                    sliderInput("h", label="Frequency", min = 0, max = 10, value= 1, width="100%", step = 0.1),
+                    width=10, offset=1)
+                    )
                 )
 
 server <- function(input, output, session){
     output$plot1 <- renderPlot(
         plot(t,mySin(t, h=input$h),
-             type="l", ylab="Amplitude")
+             type="l", ylab="Amplitude", main = sprintf("%.1f Hz", input$h))
     )
 }
 
